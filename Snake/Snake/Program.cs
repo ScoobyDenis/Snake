3a7c7008a1f15d3fd20b426490f12d79e9f61773
Pavlov Denis <denis_pavlov_20155@mail.ru>
2022-08-06 11:37:09 +0300

    add less3

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Point
+{
+    public int x;
+    public int y;
+    public char sym;
+    public void Draw()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(sym);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p1 = new Point();
+        p1.x = 1;
+        p1.y = 3;
+        p1.sym = '*';
+
+        Point p2 = new Point();
+        p2.x = 4;
+        p2.y = 5;
+        p2.sym = '#';
+
+        p1.Draw();
+        p2.Draw();
+
+        Console.ReadLine();
+    }
+}
